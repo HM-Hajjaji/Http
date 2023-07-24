8aa61f79389342663b04610f2529d615d44af098
HM-Hajjaji <hhajjaji.fsd@gmail.com>
2023-07-24 01:05:43 +0100

    add function request to send request by url and method

--- src/Request.php
+++ src/Request.php
@@ -21,4 +21,40 @@ class Request
         $this->file = new File($_FILES);
         $this->cookie = new Cookie($_COOKIE);
     }
+
+    public function request(string $url,string $method = "GET"|"POST",array $params = []): Response
+    {
+        if (!filter_var($url,FILTER_VALIDATE_URL))
+        {
+            throw new \InvalidArgumentException(sprintf('Expected a scalar, or an array as a 2nd argument to "%s()", "%s" given.', __METHOD__, get_debug_type($url)));
+        }
+
+        $object = curl_init();
+
+        curl_setopt($object,CURLOPT_URL,$url);
+        curl_setopt($object, CURLOPT_RETURNTRANSFER, true);
+
+        if (!empty($params))
+        {
+            curl_setopt($object,CURLOPT_POSTFIELDS,http_build_query($params));
+        }
+
+        if ($method == "GET")
+        {
+            curl_setopt($object,CURLOPT_HTTPGET,true);
+        }else{
+            curl_setopt($object,CURLOPT_POST,true);
+        }
+
+        $content = curl_exec($object);
+
+        if(curl_errno($object)){
+            $error = curl_error($object);
+            curl_close($object);
+            throw new \HttpRequestException($error);
+        }
+
+        curl_close($object);
+        return new Response($content);
+    }
 }
